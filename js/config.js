24901cd743c2e539017074e67a2aa1ef92718fdf
Ayyappa231 <ayyappathirumalasetty12@gmail.com>
2026-08-15 12:53:01 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Paste your deployed Google Apps Script Web App /exec URL here.
 const RESPONSE_API_URL =
-    "https://script.google.com/macros/s/AKfycbzFi_3Yk8oij0paucdZIs37_w_v2BQNTx6_UrPurzjgouhrnkhbqOAiOHXSZEwrbRVbBw/exec";
+    "https://script.google.com/macros/s/AKfycbxkr1grHF-KWDND6bLC7_3L2GB8fmhZOpnfwBa6c-M8PWXq8r2CFOpPFPo5DUBcxNlxEg/exec";
